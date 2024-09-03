d52920b0c7cdb92657b56acaf5d5ee5124a7ec7a
Jurannn <juuurr@naver.com>
2024-09-03 14:05:47 +0900

    update settings

--- fisa_django/fisa_django/settings.py
+++ fisa_django/fisa_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5@a^!@%32828y-k8@38e=h64syp^-r1+g1$&ch)phjtef^$6+#"
+SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
